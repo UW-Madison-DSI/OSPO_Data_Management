c69e2d4f55c325a1e07d41cecf0975ea80be83b0
Simon <simon.j.goring@gmail.com>
2025-02-20 10:49:24 -0800

    gitflow-feature-stash: documentation
    Added a test file for the clean_names with some assorted URLs.

---
+++ xddsource/tests/clean_repo_test.py
@@ -0,0 +1,16 @@
+from gddospo.ospo_db_tools import clean_repo_name
+import re
+
+def cleaning_names():
+    NAMES = ['htp:/github.cam/robot',
+             'robot/package toast.',
+             'https://github.com/harrietlau/ Complex- Viscosity',
+             'http://github.com/SimonGoring/repo',
+             'http://github.com/SimonGoring/repo,',
+             'http://github.com/SimonGoring/repo.',
+             'http://github.com/SimonGoring/repo ',
+             None]
+    cleaned = [clean_repo_name(i) for i in NAMES]
+    assert len([i for i in cleaned if i]) == len(NAMES) - 1, "None values are not being passed properly."
+    assert all([re.match('^https://', i) for i in cleaned if i]), "'https:// is not being prepended properly."
+    assert all([len(i) == len(i.strip()) for i in cleaned if i]), "Whitespace is not being properly removed."
